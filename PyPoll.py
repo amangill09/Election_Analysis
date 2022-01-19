6789b0fec705039372e3b33acf7415d49bc1d28f
Aman Gill <amangill09@gmail.com>
2022-01-18 16:44:20 -0800

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -9,9 +9,11 @@
 # assign a variable for the file to load and the path
 # file_to_load = 'Resources\election_results.csv'
 # election_data = open(file_to_load,'r')
+from asyncore import write
 import csv
 from functools import total_ordering
 import os
+from textwrap import wrap
 
 file_to_load = os.path.join("resources","election_results.csv")
 file_to_save = os.path.join("analysis","election_results.csv")
@@ -23,6 +25,7 @@ candidate_options = []
 # initialize dictionary for candidate votes
 candidate_votes = {}
 # winning_count and winning percentage
+winner_name = ""
 winning_count = 0
 winning_percentage = 0.00
 
@@ -44,25 +47,46 @@ with open(file_to_load,"r") as election_data:
             # add name to 
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] += 1
-        
+
+with open(file_to_save, "w") as txt_file:
+    election_results = (
+        f"\nElection Resuls\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"    
+        f"-------------------------\n\n"
+                        )
+    print(election_results, end="")
+    #save to text file
+    txt_file.write(election_results)    
+    
+    
 #print(total_votes)
 #print(candidate_options)    
 #print(candidate_votes)   
 
-for i in candidate_votes:
-    # 2. Retrieve vote count of a candidate.
-    votes = candidate_votes[i]
-    # 3. Calculate the percentage of votes.
-    vote_percentage = float(votes) / float(total_votes) * 100
-    # 4. Print the candidate name and percentage of votes.
-    print(f"{i} received {vote_percentage:.1f}% of the vote with a total of {votes:,} votes")
-    # 5. Loop to check current candidate's vote and percentage against highest winning number (initially set to 0). 
-    if votes > winning_count and vote_percentage > winning_percentage:
-        winning_count = votes
-        winning_percentage = vote_percentage
-        winner_name = i
+    for i in candidate_votes:
+        # 2. Retrieve vote count of a candidate.
+        votes = candidate_votes[i]
+        # 3. Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+        # 4. Print the candidate name and percentage of votes.
+        #print(f"{i} received {vote_percentage:.1f}% of the vote with a total of {votes:,} votes")
+        candidate_results = (f"{i}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_results)
+        txt_file.write(candidate_results)
+        # 5. Loop to check current candidate's vote and percentage against highest winning number (initially set to 0). 
+        if votes > winning_count and vote_percentage > winning_percentage:
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winner_name = i
+    # after for loop to print summary (and write to file)
+    winning_candidate_summary = f"\n---------------------\nWinner: {winner_name}\nWinning Vote Count: {winning_count:,}\nWinning percentage: {winning_percentage:,.1f}%\n-------------------"
+    print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
 
-print(f'---------------------\nWinner: {winner_name}\nWinning Vote Count: {winning_count:,}\nWinning percentage: {winning_percentage:,.1f}%')
+#print(f'---------------------\nWinner: {winner_name}\nWinning Vote Count: {winning_count:,}\nWinning percentage: {winning_percentage:,.1f}%')
+
+#candidate_results = (f'---------------------\nWinner: {winner_name}\nWinning Vote Count: {winning_count:,}\nWinning percentage: {winning_percentage:,.1f}%')
 
 # close the file
 election_data.close()
